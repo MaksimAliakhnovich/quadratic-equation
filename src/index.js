3e27c4ddd6799cee31340dbd84881d06fabecd07
AliakhnovichM <maksim.aliakhnovich@gmail.com>
2018-09-26 00:35:45 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
-// module.exports = function solveEquation(equation) {
-function solveEquation() {
-  const a = -20;
-  const b = -108797540;
-  const c = -130011773690520;
+module.exports = function solveEquation(equation) {
+  const prepEquation = equation.replace(/\s/g, ""); // удаляем пробелы
+
+  /* коэфф. перед х^2, сразу берём из массива */
+  const a = prepEquation.match(/(-?\d+)(?:\*x\^2)/)[1];
+
+  /* коэфф. после х^2, хотя лучше было бы искать перед х. Сразу берём из массива */
+  const b = prepEquation.match(/(?:x\^2\+?)(-?\d+)/)[1];
+  
+  /* свободный коэфф., сразу берём из массива */
+  const c = prepEquation.match(/-?\d+$/)[0];
+
   const discriminant = Math.pow(b, 2) - 4 * a * c;
   const arrOfRoots = [];
 
   if (discriminant == 0) {
-    let root = -b / (2 * a);
+    const root = -b / (2 * a);
     arrOfRoots.push(root);
     arrOfRoots.push(root);
   } else if (discriminant > 0) {
-    let rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
-    let rootTwo = (-b - Math.sqrt(discriminant)) / (2 * a);
+    const rootOne = (-b + Math.round(Math.sqrt(discriminant))) / (2 * a);
+    const rootTwo = (-b - Math.round(Math.sqrt(discriminant))) / (2 * a);
     arrOfRoots.push(rootOne);
     arrOfRoots.push(rootTwo);
   }
 
   return arrOfRoots.sort((a, b) => a - b);
 }
-
-solveEquation();
